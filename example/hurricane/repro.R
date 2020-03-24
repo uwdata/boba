cce2796b84a82ed6e68ab8bc12800f20146dc33e
Yang Liu <yliu0@cs.washington.edu>
2020-03-23 20:55:26 -0700

    Create a new specification (repro.R) where we make the exact same decisions as prior work

--- example/hurricane/repro.R
+++ example/hurricane/repro.R
@@ -5,10 +5,13 @@
     {"var": "outliers", "options": [
         "c()",
         "c('Katrina')",
-        "c('Katrina', 'Audrey')",
-        "c('Katrina', 'Audrey', 'Sandy')",
-        "c('Katrina', 'Audrey', 'Sandy', 'Andrew')",
-        "c('Katrina', 'Audrey', 'Sandy', 'Andrew', 'Donna')"
+        "c('Katrina', 'Audrey')"
+    ]},
+    {"var": "leverage_points", "options": [
+        "c()",
+        "c('Sandy')",
+        "c('Sandy', 'Andrew')",
+        "c('Sandy', 'Andrew', 'Donna')"
     ]},
     {"var": "feminity", "options": ["female", "masfem"]},
     {"var": "feminity_prediction_levels", "options": ["c(0, 1)", "c(2.53, 8.29)"]},
@@ -41,13 +44,11 @@
     ]},
     {"var": "back_transform", "options": [
       "exp(mu + sigma^2/2) - 1",
-      "mu",
-      "exp(mu + sigma^2/2) - 1"
+      "mu"
     ]},
     {"var": "df", "options": [
         "inference$df",
-        "df.residual(model)",
-        "inference$df"
+        "df.residual(model)"
     ]}
   ],
   "constraints": [
@@ -136,6 +137,7 @@ df <- read_csv('../data.csv',
     ) %>%
     # remove outliers
     filter(!(name %in% {{outliers}})) %>%
+    filter(!(name %in% {{leverage_points}})) %>%
     # operationalize feminity
     mutate(
         feminity = {{feminity}},
@@ -152,11 +154,6 @@ fit = cross(df, lm, log_death ~ {{predictors}} {{covariates}}) # cross validatio
 model <- glm.nb(death ~ {{predictors}} {{covariates}}, data = df)
 fit = cross(df, glm.nb, death ~ {{predictors}} {{covariates}}) # cross validation
 
-# --- (M) anova
-# ANOVA with log(deaths+1) as the dependent variable
-model <- aov(log_death ~ {{predictors}} {{covariates}}, data = df)
-fit = cross(df, aov, log_death ~ {{predictors}} {{covariates}}) # cross validation
-
 # --- (O)
 # normalize RMSE
 nrmse = fit / (max(df$death) - min(df$death))
@@ -168,7 +165,7 @@ inference_df <- df %>%
     ungroup()
 
 # get inferential fit from model
-inference <- predict(model, inference_df, se.fit = TRUE, type = "response") 
+inference <- predict(model, inference_df, se.fit = TRUE, type = "response")
 inference_fit <- inference_df %>%
     mutate(
         fit = inference$fit,                            # add inferential fits and standard errors to dataframe
@@ -180,9 +177,9 @@ inference_fit <- inference_df %>%
 
 # aggregate fitted effect of female storm name
 expectation <- inference_fit %>%
-    mutate(expected_deaths = pred2expectation(fit, sigma)) %>% 
+    mutate(expected_deaths = pred2expectation(fit, sigma)) %>%
     group_by(feminity) %>%                                            # group by predictor(s) of interest
-    summarize(expected_deaths = weighted.mean(expected_deaths)) %>% # marninalize across other predictors       
+    summarize(expected_deaths = weighted.mean(expected_deaths)) %>% # marninalize across other predictors
     compare_levels(expected_deaths, by = feminity) %>%
     ungroup() %>%
     dplyr::select(expected_diff = expected_deaths) %>%
@@ -191,16 +188,16 @@ expectation <- inference_fit %>%
 # propagate uncertainty in fit to model predictions
 uncertainty <- inference_fit %>%
     mutate(
-        .draw = list(1:5000),                               # generate list of draw numbers
-        t = map(df, ~rt(5000, .)),                          # simulate draws from t distribution to transform into means
-        x = map(df, ~rchisq(5000, .))                       # simulate draws from chi-squared distribution to transform into sigmas
+        .draw = list(1:200),                               # generate list of draw numbers
+        t = map(df, ~rt(200, .)),                          # simulate draws from t distribution to transform into means
+        x = map(df, ~rchisq(200, .))                       # simulate draws from chi-squared distribution to transform into sigmas
     ) %>%
     unnest(cols = c(".draw", "t", "x")) %>%
     mutate(
         mu = t * se.fit + fit,                              # scale and shift t to get a sampling distribution of means
         sigma = sqrt(df * se.residual^2 / x),               # scale and take inverse of x to get a sampling distribution of sigmas
         expected_deaths = pred2expectation(mu, sigma)
-    ) %>%        
+    ) %>%
     group_by(.draw, feminity) %>%                             # group by predictor(s) of interest
     summarize(expected_deaths = mean(expected_deaths)) %>%  # marninalize across other predictors
     compare_levels(expected_deaths, by = feminity) %>%
@@ -208,7 +205,7 @@ uncertainty <- inference_fit %>%
     dplyr::select(expected_diff = expected_deaths)
 
 # get predictive check for original dataset from model
-pred <- predict(model, df, type = "response") 
+pred <- predict(model, df, type = "response")
 disagg_fit <- df %>%
     mutate(
         fit = pred,                                 # get fitted predictions
