3f415eca28d7d83245f21cd79b76f1ce85e70f26
Yang Liu <yliu0@cs.washington.edu>
2020-04-19 16:19:16 -0700

    Minor

--- example/mortgage/after_execute.sh
+++ example/mortgage/after_execute.sh
@@ -1,6 +1,3 @@
 cd ./multiverse
 boba merge estimate_{}.csv -b ./results --out estimate.csv
 boba merge uncertainty_{}.csv -b ./results --out uncertainty.csv
-boba merge null_{}.csv -b ./results --out null.csv
-
-Rscript stacking_weights.R
