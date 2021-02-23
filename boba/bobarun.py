d67c91c7a568184c4f8aa4163555e082ad78c1a5
Yang Liu <yliu0@cs.washington.edu>
2021-02-23 14:55:05 -0800

    Refactor boba run

--- boba/bobarun.py
+++ boba/bobarun.py
@@ -1,8 +1,83 @@
 import subprocess
+import pandas as pd
 import os
-from .lang import Lang
-from .wrangler import DIR_SCRIPT, DIR_LOG, get_universe_id_from_script, get_universe_log, get_universe_error_log, get_universe_name
+import json
+import multiprocessing as mp
 from subprocess import PIPE
+from .lang import Lang
+from .wrangler import *
+
+
+def run_multiverse(folder, run_all, num, thru, jobs, batch_size):
+    """ Entry point of boba run """
+    # read summary
+    data = pd.read_csv(folder + '/summary.csv')
+    vals = data['Filename'].to_list()
+
+    # language
+    try:
+        with open(folder + '/lang.json', 'r') as f:
+            lang = Lang(vals[0], supported_langs=json.load(f))
+    except IOError:
+        lang = Lang(vals[0])
+
+    # get the names of all the universes we want to run
+    universes = []
+    extension = lang.get_ext()
+    if run_all:
+        universes = vals
+    else:
+        if thru == -1:
+            thru = num
+        for i in range(num, thru + 1):
+            universe = get_universe_script(i, extension)
+            print(universe)
+            universes.append(universe)
+
+    # before execute
+    run_commands_in_folder(folder, 'pre_exe.sh')
+
+    # initialize the log folder and log file
+    p_log = os.path.join(folder, DIR_LOG)
+    if os.path.exists(p_log):
+        shutil.rmtree(p_log)
+    os.makedirs(p_log)
+
+    with open(os.path.join(p_log, 'logs.csv'), 'w') as log:
+        log.write('universe,exit_code\n')
+
+    # callback that is run for each retrieved result.
+    def check_result(r):
+        # write the results to our logs
+        with open(os.path.join(p_log, 'logs.csv'), 'a') as f_log:
+            for res in r:
+                f_log.write(res[0] + ',' + str(res[1]) + '\n')
+
+    # initialize process pool
+    if jobs == 0:
+        jobs = mp.cpu_count()
+    if batch_size == 0:
+        batch_size = min(int(len(universes)**0.5), int(len(universes)/jobs) + 1)
+
+    pool = mp.Pool(jobs)
+
+    # run each batch of universes as a separate task
+    while universes:
+        batch = []
+        while universes and len(batch) < batch_size:
+            batch.append(universes.pop(0))
+
+        pool.apply_async(run_batch_of_universes,
+            args=(folder, batch, lang.supported_langs),
+            callback=check_result)
+
+    # collect all the results
+    pool.close()
+    pool.join()
+
+    # after execute
+    run_commands_in_folder(folder, 'post_exe.sh')
+
 
 def run_batch_of_universes(folder, universes, supported_langs):
     """ Run a batch of universes """
--- boba/cli.py
+++ boba/cli.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """Console script."""
-import json
 import click
 import shutil
+import os
+import pandas as pd
 from .parser import Parser
 from .output.csvmerger import CSVMerger
-import multiprocessing as mp
-import pandas as pd
-from .bobarun import *
-from .wrangler import get_universe_script, DIR_LOG
+from .bobarun import run_multiverse
 
 
 @click.command()
@@ -73,78 +71,20 @@ def run(folder, run_all, num, thru, jobs, batch_size):
 
     check_path(folder)
 
-    # get the names of all the universes we want to run
-    universes = []
-    data = pd.read_csv(folder + '/summary.csv')
-    vals = data['Filename'].to_list()
-    
-    try:
-        with open(folder + '/lang.json', 'r') as f:
-            lang = Lang(vals[0], supported_langs=json.load(f))
-    except IOError:
-        lang = Lang(vals[0])
+    df = pd.read_csv(folder + '/summary.csv')
+    num_universes = df.shape[0]
 
-        
-    extension = lang.get_ext()
-    if run_all:
-        universes = vals
-    else:
+    if not run_all:
         if thru == -1:
             thru = num
         if num < 1:
             print_help()
         if thru < num:
             print_help('The thru parameter cannot be less than the num parameter.')
-        if num > len(vals) or thru > len(vals):
-            print_help('There are only ' + str(len(vals)) + ' universes.')
+        if num > num_universes or thru > num_universes:
+            print_help(f'There are only {num_universes} universes.')
 
-        for i in range(num, thru + 1):
-            universe = get_universe_script(i, extension)
-            print(universe)
-            universes.append(universe)
-
-    run_commands_in_folder(folder, 'pre_exe.sh')
-
-    if jobs == 0:
-        jobs = mp.cpu_count()
-
-    if batch_size == 0:
-        batch_size = min(int(len(universes)**0.5), int(len(universes)/jobs) + 1)
-
-    pool = mp.Pool(jobs)
-
-    results = []
-
-    p_log = os.path.join(folder, DIR_LOG)
-    if os.path.exists(p_log):
-        shutil.rmtree(p_log)
-    os.makedirs(p_log)
-
-    with open(os.path.join(p_log, 'logs.csv'), 'w') as log:
-        log.write('universe,exit_code\n')
-
-    # callback that is run for each retrieved result.
-    def check_result(r):
-        results.extend(r)
-        # write the results to our logs
-        with open(os.path.join(p_log, 'logs.csv'), 'a') as f_log:
-            for res in r:
-                f_log.write(res[0] + ',' + str(res[1]) + '\n')
-
-
-    # run each batch of universes as a separate task
-    while universes:
-        batch = []
-        while universes and len(batch) < batch_size:
-            batch.append(universes.pop(0))
-
-        pool.apply_async(run_batch_of_universes, args=(folder, batch, lang.supported_langs), callback=check_result)
-
-    # collect all the results
-    pool.close()
-    pool.join()
-
-    run_commands_in_folder(folder, 'post_exe.sh')
+    run_multiverse(folder, run_all, num, thru, jobs, batch_size)
 
 
 @click.command()
